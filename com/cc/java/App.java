3bf6d690719ac016d6bc46b6ffb32cf93ad6c2b2
Peter <schmidt.multimedia@gmail.com>
2021-10-26 11:32:40 +0200

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,23 +5,9 @@ public class App {
     public static void main(String[] args) {
 
       Customer customer = new Customer(1, "Maxine Mütze", "Mützenhausen");  
-      Helper.ausgabe(customer.getName());
-      Helper.ausgabe(customer.getCity());
+      customer.initOrder();
       
-     /* 1. Typkonvertierung */
-      // Helper.ausgabe(String.valueOf(customer.getId()));
-
-      /* 2. Aufruf einer überladenen Methode */ 
-      Helper.ausgabe(customer.getId());
-      Helper.ausgabe("-------------");
-
-
-      // Test der Klasse
-      Order order = new Order(1, "repair");
-      Helper.ausgabe(order.getOrderID());
-      Helper.ausgabe(order.getOrderType());
-      Helper.ausgabe(order.isFinished());
-      Helper.ausgabe(order.getOrderDate());
+   
 
     }
 
